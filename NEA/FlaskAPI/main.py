9b9e38e8c56e9ca365dc0c723daa159f07de39eb
Acer Triton 300 <joejoecrawley@gmail.com>
2024-10-21 17:32:44 +0100

    added API and app multi-frame controller

--- NEA/FlaskAPI/main.py
+++ NEA/FlaskAPI/main.py
@@ -11,12 +11,22 @@ app = Flask(__name__)
 
 #POST is giving some data to the server in exchange for some more (sometimes)
 
+#Tasks
 
+#Research flask API's understand what a POST method is
+
+#Implement a login through your API (simple if statements for now, SQL later)
 
 @app.route('/')     #My first endpoint (Where someone gets something)
 def hello_world():
     return 'hello forld !'
+@app.route('/login',methods=['POST'])
+def login():
 
+    if password =='Password123':
+        return True
+    #Do this !
+    pass
 @app.route('/Multiplyby5',methods=['GET'])
 def Multiply():
     data_id = request.args.get('input')
