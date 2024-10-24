662b65bddde872172a2da9a846312eff6996ff94
Jaques108 <jakemgalvin@gmail.com>
2024-10-24 23:06:14 +0100

    Upload

--- NEA/FlaskAPI/Data.py
+++ NEA/FlaskAPI/Data.py
@@ -1,6 +1,9 @@
 from crypt import methods
-
 from flask import Flask,request,jsonify
+from bs4 import BeautifulSoup
+import json
+import requests
+from tkinter import *
 
 
 app = Flask(__name__)
@@ -9,7 +12,7 @@ app = Flask(__name__)
 
 #The ones we care about are GET and POST
 
-#Get is getting something quickly
+#GET is getting something quickly
 
 #POST is giving some data to the server in exchange for some more (sometimes)
 
@@ -19,33 +22,19 @@ app = Flask(__name__)
 
 #Implement a login through your API (simple if statements for now, SQL later)
 
-@app.route('/fun')     #My first endpoint (Where someone gets something)
-def hello_world():
-    return 'hello world !'
-
-@app.route('/login',methods=['POST'])
-def login():
-
-    if password =='Password123':
-        return True
-    #Do this !
-    pass
-@app.route('/Multiplyby5',methods=['GET'])
-def Multiply():
-    data_id = request.args.get('input')
-    if data_id:
-        return f'{int(data_id) * 5}'
-    else:
-        return 'no data !'
-app.run(debug=True)
+#My first endpoint (Where someone gets something)
 
 
+@app.route('/GenerateCells',methods=['GET'])
+def crossword():
 
-
-@app.route('/GenerateCells',methods=['POST'])
-def generateCells(code):
-    cells = []
+    genericUrl = "https://www.theguardian.com/crosswords/quick/"
+    code = "16992"
     url = genericUrl + code
+
+
+    cells = {}
+
 
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
@@ -54,6 +43,7 @@ def generateCells(code):
 
     jsonified = json.loads(test)
 
+
     startPositions = []
     seen = set()
     for item in jsonified['entries']:
@@ -61,7 +51,7 @@ def generateCells(code):
         tempX = temp['x']
         tempY = temp['y']
 
-        startPosition = [tempX, tempY]
+        startPosition = (tempX, tempY)
 
         # Removes duplicates. This is because some start positions belong to both across and down words.
         if startPosition not in seen:
@@ -89,58 +79,66 @@ def generateCells(code):
             else:
                 obj = '⬛'
 
-            cell = Label(playWinCrossword, text=obj, justify='center')
-            cell.grid(row=row, column=col, padx=4, pady=4)
-            cells.append(cell)
+
+            position = str(row) + str(col)
+            cells[position] = {'text':obj}
 
 
-    cellsBelongingToWord()
 
+    cellsBelongingToWord(jsonified,cells)
     return cells
 
 
-
-
-
-
-def cellsBelongingToWord():
-    for item in startPositions:
-        pos = startPositions[item]
-        posY = pos[0]
-        posX = pos[1]
+def cellsBelongingToWord(var,cells):
+    for item in var['entries']:
+        temp = item['position']
+        posX = temp['x']
+        posY = temp['y']
 
         direction = item['direction']
         length = item['length']
 
+
         if direction == 'across':
             for x in range(1, length):
-                cell = cells[i + x][0]
-                text = cell.cget(key="text")
+                position = str(posY) + str(posX + x)
 
-                if text == "S":
+                cell = cells.get(position)
+                text = cell.get('text')
+
+                if text == "⬛":
+                    cell['text'] = "--"
+
+                elif text == "S":
                     pass
 
-                elif text == "|":
-                    cell.config(text="□")
-
                 else:
-                    cell.config(text="--")
+                    cell['text'] = "□"
 
 
         else:
-            for n in range(1, (Length * 13), 13):
-                cell = cells[i + n][0]
-                text = cell.cget(key="text")
+            for n in range(1, length):
+                position = str(posY + n) + str(posX)
+                cell = cells.get(position)
+                text = cell.get('text')
 
-                if text == "S":
+
+                if text == "⬛":
+                    cell['text'] = "|"
+
+                elif text == "S":
                     pass
 
-                elif text == "--":
-                    cell.config(text="□")
-
                 else:
-                    cell.config(text="|")
+                    cell['text'] = "□"
 
 
-        break
+app.run(debug=True)
+
+
+
+
+
+
+
 
