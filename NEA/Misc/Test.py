acfdcf961cfcbaced4d2b6be640d1809e47ac642
Jaques108 <jakemgalvin@gmail.com>
2024-10-24 22:15:37 +0100

    Upload

--- NEA/Misc/Test.py
+++ NEA/Misc/Test.py
@@ -2,18 +2,4 @@
 # Please delete when done :)
 
 
-# Step 1: Initialize an empty dictionary
-my_dict = {}
-
-# Step 2: Define a key for the new item
-item_key = "position"
-
-# Step 3: Create a value that is a dictionary
-new_item = '40'
-
-# Step 4: Add the new item to the dictionary
-my_dict[item_key] = new_item
-
-# Print the updated dictionary
-print(my_dict['position'])
 
